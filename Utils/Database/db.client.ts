f1ba400d082c461669ca3bc676aaa0e5d9e8bee8
Bhadiyadra Sarthak <bhadiyadrasarthak2002@gmail.com>
2023-07-14 16:37:37 +0530

    empty code

--- Utils/Database/db.client.ts
+++
@@ -1,4 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-const nsql_db: PrismaClient = new PrismaClient({ datasources: { db: { url: process.env.NS_DATABASE_URL } } });
-export { nsql_db };
--- build/Utils/Database/db.client.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.nsql_db = void 0;
-const client_1 = require("@prisma/client");
-const nsql_db = new client_1.PrismaClient({ datasources: { db: { url: process.env.NS_DATABASE_URL } } });
-exports.nsql_db = nsql_db;
